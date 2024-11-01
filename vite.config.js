56ec6ea62c92fea6aad5414cddb7ac5a798664b2
yanicorn3000 <yanadziabiola@gmail.com>
2024-11-01 13:44:34 +0100

    setup build

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,14 @@ const exercisePath = "src";
 /**
  * Don't change those lines below
  */
-export default defineConfig({
+export default defineConfig((config) => ({
+  base: config.mode === "production" ? "/todoList" : "/",
   root: exercisePath,
   server: {
     port: 3000,
   },
   plugins: [react()],
-});
+  build: {
+    outDir: "../dist",
+  },
+}));
